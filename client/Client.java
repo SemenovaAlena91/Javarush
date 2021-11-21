c4c668124efa02b27616aaadefd9f44c02e378cc
Alena Semenova <semenova.alena91@mail.ru>
2021-11-21 22:18:39 +0300

    added methods to class Client

--- client/Client.java
+++ client/Client.java
@@ -1,10 +1,51 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Введите адрес");
+        String address = ConsoleHelper.readString();
+        return address;
+    }
+
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Введите порт сервера");
+        int port = ConsoleHelper.readInt();
+        return port;
+    }
+
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Введите имя пользователя");
+        String userName = ConsoleHelper.readString();
+        return userName;
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected void sendTextMessage(String text){
+        try{
+            Message message = new Message(MessageType.TEXT, text);
+            connection.send(message);
+        }catch (IOException e){
+            clientConnected = false;
+        }
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
     public class SocketThread extends Thread{
 
     }
